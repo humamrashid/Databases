73a24cd8e57e4e4406a14a09789dd978ce64e1da
Humam Rashid <humamrashid@gmail.com>
2019-09-17 15:10:49 -0400

    Added SQL notes.

---
+++ Notes.sql
@@ -0,0 +1,263 @@
+-- Notes on SQL, not actual SQL code!
+
+-- SQL
+
+-- DDL (Data Definition Language).
+    create table;
+    drop table;
+-- DML (Data Manipulation Language).
+-- Used to perform CRUD (Create, Retrieve, Update and Delete) operations which
+-- all databases are expected to support.
+    insert
+    select
+    update
+    delete
+
+-- Creating tables.
+
+create table customer (
+    custid bigint,
+    name varchar(100),
+    email varchar(100),
+    dob date,
+    street varchar(100),
+    city varchar(100),
+    state varchar(2),
+    zip varchar(5)
+);
+
+-- Inserting data.
+
+-- Inserts are relatively slow, requiring disk access before the database can be
+-- updated. When adding multiple data units, use database utilities specific to
+-- the DBMS you're using.
+insert int customer(custid, name, email)
+    values(1, 'John Doe', 'john.doe@gmail.com');
+insert int customer(custid, name, email)
+    values(2, 'Jane Doe', 'jane.doe@gmail.com');
+
+-- Be careful when updating data, don't use if not sure, prefer adding new
+-- address.
+update customer set dob='2001-01-01' where custid=1;
+update customer set zip='10001' where custid=1;
+
+-- Be careful when deleting data, don't use if not sure.
+delete from customer where custid=2;
+
+-- Don't treat customer data as a state, rather consider it as a series of
+-- transactions.
+
+-- Querying data.
+
+-- Select statements.
+
+-- Getting everything/everyone:
+
+select * from customer;
+
+-- Getting a single customer:
+select * from customer where custid=2;
+
+-- Get customers name 'Doe'.
+select * from customer where name line '%Doe';
+
+-- Get count of customers.
+select count(*) from customer;
+
+-- Get count of customers named 'Doe'.
+select count(*) from customer where name like '%Doe';
+
+-- Get count of customers by state
+select state, count(*)
+from customer
+group by state
+order by desc;
+
+-- Get percentage of customers in NY.
+select 100 * sum(case when state='NY' then 1.0 else 0.0 end) / sum(1.0)
+from customer
+
+-- Create age group label.
+select a.*,
+    case when dob >='2001-01-01' then 'TEEN'
+         when dob >='1970-01-01' then 'MID'
+         when dob >= '1950-01-01' then 'ELD'
+        else 'SNR'
+    end as a egegrp
+from customer a;
+
+-- Count customers by age group.
+-- Using CTEs: Common Table Expressions.
+
+with agestmt as (
+    select a.*,
+        case when dob >='2001-01-01' then 'TEEN'
+            when dob >='1970-01-01' then 'MID'
+            when dob >= '1950-01-01' then 'ELD'
+            else 'SNR'
+        end as a egegrp
+    from customer a
+)
+select agegrp, count(*)
+from agestmt
+group by agegrp
+order by 2 desc;
+
+-- Everything starting with 'with' to 'order by...' above is a single select
+-- statement.
+
+-- Creating tables from data.
+
+create table cust_agegrp as
+    select a.*,
+        case when dob >='2001-01-01' then 'TEEN'
+            when dob >='1970-01-01' then 'MID'
+            when dob >= '1950-01-01' then 'ELD'
+            else 'SNR'
+        end as a egegrp
+from customer a;
+
+select * from cust_agegrp where agegrp='TEEN';
+
+-- Creating views from queries.
+
+create table cust_agegrp as
+    select a.*,
+        case when dob >='2001-01-01' then 'TEEN'
+            when dob >='1970-01-01' then 'MID'
+            when dob >= '1950-01-01' then 'ELD'
+            else 'SNR'
+        end as a egegrp
+from customer a;
+
+select * from cust_agegrp where agegrp='TEEN';
+
+select agegrp, count(*)
+from cust_agegrp
+group by agegrp
+order by desc;
+
+-- Joins.
+
+-- Example, tables for what your purchased.
+create table purchase (
+    purchaseid bigint,
+    custid bigint,
+    tim timestamp
+);
+
+create table purchase_detail (
+    detailid bigint,
+    purchaseid bigint,
+    productid bigint,
+    qty int,
+    price numeric(18, 8)
+);
+
+-- Get all purchases for custid=1;
+select * from purchase where custid=1;
+
+-- Get all purchases for customers in zip 10001.
+select a.*
+from purchase a
+    inner join customer b
+    on a.custid=b.custid
+where zip='10001';
+
+-- customer.custid is 'primary' key.
+-- purchase.custid is 'foreign' key.
+
+-- Count number of purchases by customers in zip 10001.
+
+select count(*)
+from purchase a
+    inner join customer b
+    on a.custid=b.custid
+where zip='10001';
+
+-- purchase counts by state
+select count(*)
+    inner join purchase b
+    on a.custid=b.custid
+group by state
+order by 2 desc;
+
+-- How many of each product did 'John Doe' who lives in NY purchase?
+select productid, sum(c.qty)
+from customer
+    inner join purchase b
+    on a.custid=b.custid
+    inner join purchase_detail c
+    on b.purchaseid=c.purchaseid
+where a.name='John Doe' and a.state='NY'
+group by productid
+order by 2 desc;
+
+-- "natural joins": use all columns that are named the same.
+-- These can be dangerous, use with caution.
+select productid, sum(c.qty)
+from customer
+    natural inner join purchase b --custid is join column
+    natural inner join purchase_detail c -- purchaseid
+where a.name='John Doe' and a.state='NY'
+group by productid
+order by 2 desc;
+
+-- Few types of joins.
+
+inner join
+
+select ... from tableA inner join tableB on ...
+-- records in both tableA and tableB have to exist to get a row of output.
+
+left outer join
+
+select ... from tableA left outer join tableB on ...
+-- all records from tableA, with potential matches in tableB.
+
+select custid, purchaseid
+from customer a
+    left outer join purchase b
+    on a.custid=b.custid
+
+-- output, e.g.:
+
+1, 2135
+1, 2324
+2, NULL
+3, 7350
+
+select ... from tableA right outer join tableB on ...
+-- all records from tableB, with potential matches in tableA.
+
+full outer join
+-- essentially a combination of left and right outer joins.
+
+select custid, purchaseid
+from customer a
+    full outer join purchaes b
+    on a.custid=b.custid
+
+-- output, e.g.:
+
+1, 2135
+1, 2324
+2, NULL
+3, 7350
+NULL, 5178
+NULL, 5312
+
+-- Getting customers who've never purchased anything.
+select ...
+from customer a
+    left outer join purchase b
+    on a.custid=b.custid
+where b.purchaseid is null
+
+-- Get customers who did purchase something.
+select distinct a.custid
+from customer a
+    inner join purchase b
+    on a.custid=b.custid
+
+-- EOF.
