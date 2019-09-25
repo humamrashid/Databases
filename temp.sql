c0e6e91bce1d94700b659b3c3ed68291bd7e3401
Humam Rashid <humamrashid@gmail.com>
2019-09-25 02:22:21 -0400

    Incremental commit.

--- temp.sql
+++ temp.sql
@@ -14,7 +14,7 @@ FROM in_bathroom;
 
 /* Question 3. */
 WITH in_building AS (
-    SELECT eventid FROM dooorlog
+    SELECT eventid FROM doorlog
     WHERE (doorid=1 OR doorid=3) AND event='E'
     MINUS
     SELECT eventid FROM doorlog
@@ -22,3 +22,17 @@ WITH in_building AS (
 )
 SELECT COUNT(*)
 FROM in_building;
+
+/* Question 4. */
+WITH in_buildin0704 AS (
+    SELECT eventid FROM doorlog
+    WHERE
+        ((doorid=1 OR doorid=3) AND event='E')
+        AND tim <'2019-07-04 22:00:00'
+    MINUS
+    SELECT eventid FROM doorlog
+    WHERE ((doorid=1 OR doorid=3) AND event='X')
+        AND tim >='2019-07-04 22:00:01'
+)
+SELECT COUNT(*)
+FROM in_building0704;
