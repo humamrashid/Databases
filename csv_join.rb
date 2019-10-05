57084d32b80426b6fee7c0bebc3681acde6a7676
Humam Rashid <humamrashid@gmail.com>
2019-10-04 22:26:39 -0400

    Incremental commit.

--- csv_join.rb
+++ csv_join.rb
@@ -1,30 +1,74 @@
-#!/usr/bin/ruby -w
-
-# csv_join: a join implementation for CSV files.
+#!/usr/bin/ruby
 # Humam Rashid
 # CISC 7510X, Fall 2019.
-
+#
+# csv_join: a join implementation for CSV files.
+#
+# CLI arguments:
+#   1.) < -b | -m | -n >, for join implementation type
+#   (btree, merge and nested respectively).
+#
+#   2.) <key_index>, for index of key to join on.
+#
+#   3.) <file1> <file2>, CSV files to join.
 require 'csv'
 
-def show_exit
-  abort "Usage: #{$0} < -b | -m | -n > <file1> <file2>"
-end
-
-show_exit if ARGV.length != 1
-
-def proc_args(opt)
+def proc_arg(opt)
   case opt
   when "-b"
-    #puts "got b"
+    return :btree
   when "-m"
-    #puts "got m"
+    return :merge
   when "-n"
-    #puts "got n"
+    return :nested
   else
-    show_exit
+    return nil
   end
 end
 
-ARGV.each { |a| proc_args(a) }
+def exit_if_empty(file)
+  abort "Invalid join: #{file} is empty!" if file.empty?
+end
+
+def btree_join(key_index, file1, file2)
+end
+
+def merge_join(key_index, file1, file2)
+end
+
+def nested_join(key_index, file1, file2)
+  for i in file1
+    for j in file2
+      if i[key_index] == j[key_index]
+        puts "#{i[key_index]} #{i[1..i.length].to_csv}"\
+          "#{j[1..j.length].to_csv}"
+      end
+    end
+  end
+end
+
+if ARGV.length != 4 or (choice = proc_arg(ARGV[0])).nil?
+  abort "Usage: #{$0} < -b | -m | -n > <key_index>"
+    + "<file1> <file2>"
+end
+
+file1 = CSV.read(ARGV[2])
+exit_if_empty(file1)
+file2 = CSV.read(ARGV[3])
+exit_if_empty(file2)
+
+key_index = ARGV[1].to_i
+if (key_index < 0) or (key_index >= file1[0].length)
+  abort "Join key index is out of bounds!"
+end
+
+case choice
+when :btree
+  btree_join(key_index, file1, file2)
+when :merge
+  merge_join(key_index, file1, file2)
+when :nested
+  nested_join(key_index, file1, file2)
+end
 
 # EOF.
