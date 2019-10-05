7357293c6ad45fcb91872172571e620b6f8368fa
Humam Rashid <humamrashid@gmail.com>
2019-10-05 18:37:23 -0400

    Incremental commit.

--- csv_join.rb
+++ csv_join.rb
@@ -2,21 +2,27 @@
 # Humam Rashid
 # CISC 7510X, Fall 2019.
 #
-# csv_join: a join implementation for CSV files.
+# csv_join: A join implementation for CSV files.
+# Assumes both CSV files have same number of fields per
+# record and the join key at the same position in each
+# record.
 #
 # CLI arguments:
-#   1.) < -b | -m | -n >, for join implementation type
-#   (btree, merge and nested respectively).
+#   1.) < -h | -m | -n >, for join implementation type
+#   (hash, merge and nested respectively).
 #
-#   2.) <key_index>, for index of key to join on.
+#   2.) <key_index>, for index of key to join records on,
+#   (0 to number_of_fields - 1). Each key is assumed to be
+#   unique to a record.
 #
 #   3.) <file1> <file2>, CSV files to join.
+
 require 'csv'
 
 def proc_arg(opt)
   case opt
-  when "-b"
-    return :btree
+  when "-h"
+    return :hash
   when "-m"
     return :merge
   when "-n"
@@ -30,7 +36,16 @@ def exit_if_empty(file)
   abort "Invalid join: #{file} is empty!" if file.empty?
 end
 
-def btree_join(key_index, file1, file2)
+def hash_join(key_index, file1, file2)
+  f1_htable = Hash.new
+  file1.each { |i| f1_htable.store(i[key_index].hash, i) }
+  file2.each do |j|
+    f1_val = f1_htable[j[key_index].hash]
+    puts "#{j[key_index]} #{f1_val.reject \
+      {|e| e == j[key_index]}.to_csv.chomp} #{j.reject \
+      { |e| e == j[key_index]}.to_csv.chomp}" \
+      unless f1_val.nil?
+  end
 end
 
 def merge_join(key_index, file1, file2)
@@ -40,29 +55,31 @@ def nested_join(key_index, file1, file2)
   for i in file1
     for j in file2
       if i[key_index] == j[key_index]
-        puts "#{i[key_index]} #{i[1..i.length].to_csv} #{j[1..j.length].to_csv}"
+        puts "#{i[key_index]} "\
+          "#{i.to_csv.chomp} "\
+          "#{j.to_csv.chomp}"
       end
     end
   end
 end
 
 if ARGV.length != 4 or (choice = proc_arg(ARGV[0])).nil?
-  abort "Usage: #{$0} < -b | -m | -n > <key_index> <file1> <file2>"
+  msg = "Usage: #{$0} < -h | -m | -n > <key_index>"\
+    " <file1> <file2>"
+  abort msg
 end
-
 file1 = CSV.read(ARGV[2])
 exit_if_empty(file1)
 file2 = CSV.read(ARGV[3])
 exit_if_empty(file2)
-
 key_index = ARGV[1].to_i
 if (key_index < 0) or (key_index >= file1[0].length)
   abort "Join key index is out of bounds!"
 end
 
 case choice
-when :btree
-  btree_join(key_index, file1, file2)
+when :hash
+  hash_join(key_index, file1, file2)
 when :merge
   merge_join(key_index, file1, file2)
 when :nested
