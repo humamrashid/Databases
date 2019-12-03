912f8028619e0aac6d069207771a9f1116cb0dc5
Humam Rashid <humamrashid@gmail.com>
2019-12-03 18:48:16 -0500

    Inc. commit.

--- psqldb_backup.sh
+++ psqldb_backup.sh
@@ -14,9 +14,11 @@ for ((i=0; i<=$#; i++)); do
     if [ $i -gt 4 ]; then
         PGPASSWORD=$1 psql -U $2 -d $3 -c \
         "\copy ${!i} to stdout with (format csv, header true, delimiter ',');" \
-        > $4/${!i}.csv && tar czf ${!i}.$(date '+%Y%m%d_%H%M%S').csv.tar.gz $4/${!i}.csv
-        #echo "${!i}"
+        > $4/${!i}.csv && \
+        tar czf ${!i}.$(date '+%Y%m%d_%H%M%S').csv.tar.gz $4/${!i}.csv && \
+        rm $4/${!i}.csv
     fi
 done
 exit 0
-#PGPASSWORD="A19-S1l1t" psql -U "postgres" -d "rashid_cisc7510x" -c "\copy divtab to stdout with (format csv, header true, delimiter ,);" > divtab.csv
+
+# EOF.
