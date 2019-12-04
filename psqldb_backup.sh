e4ee11853704447d4c522ecd4b4bdacf840d72a4
Humam Rashid <humamrashid@gmail.com>
2019-12-03 19:12:00 -0500

    Inc. commit.

--- psqldb_backup.sh
+++ psqldb_backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ $# -lt 5 ]; then
-    echo "usage: $0 <db_pass> <db_owner> <db_name> <backup_path> <tab1...tabN>"
+if [ $# -lt 6 ]; then
+    echo "usage: $0 <db_pass> <db_owner> <db_name> <backup_path> <recipient> <tab1...tabN>"
     exit 1
 fi
 
@@ -11,11 +11,12 @@ if [ ! -d $4 ]; then
 fi
 
 for ((i=0; i<=$#; i++)); do
-    if [ $i -gt 4 ]; then
+    if [ $i -gt 5 ]; then
         PGPASSWORD=$1 psql -U $2 -d $3 -c \
         "\copy ${!i} to stdout with (format csv, header true, delimiter ',');" \
         > $4/${!i}.csv && \
-        tar czf ${!i}.$(date '+%Y%m%d_%H%M%S').csv.tar.gz $4/${!i}.csv && \
+        tar cvz $4/${!i}.csv | gpg --encrypt --recipient $5 \
+        > $4/${!i}.$(date '+%Y%m%d_%H%M%S').tar.gz.gpg && \
         rm $4/${!i}.csv
     fi
 done
