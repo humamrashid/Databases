f599f92664221c62bb085d00d65601186576c385
Humam Rashid <humamrashid@gmail.com>
2019-12-03 19:36:33 -0500

    Inc. commit.

--- psqldb_backup.sh
+++ psqldb_backup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# psqldb_backup: a script for UNIX-like systems running bash to archive and
+# encrypt Postgresql database tables. Only works with psql and GnuPG. Further,
+# the script assumes GnuPG has been configured and a key pair already generated
+# for the recipient. Works on one database at a time but any number of tables
+# from the database can be supplied as arguments. First 5 arguments to the
+# script are database owner password, database owner name, database name, backup
+# path and recipient user-id (i.e., email address).
+
 if [ $# -lt 6 ]; then
     echo "usage: $0 <db_pass> <db_owner> <db_name> <backup_path> <recipient>"\
         "<tab1...tabN>"
@@ -16,7 +24,7 @@ for ((i=0; i<=$#; i++)); do
         PGPASSWORD=$1 psql -U $2 -d $3 -c \
         "\copy ${!i} to stdout with (format csv, header true, delimiter ',');" \
         > $4/${!i}.csv && \
-        tar cvz $4/${!i}.csv | gpg --encrypt --recipient $5 \
+        tar cz $4/${!i}.csv | gpg --encrypt --recipient $5 \
         > $4/${!i}.$(date '+%Y%m%d_%H%M%S').tar.gz.gpg && \
         rm $4/${!i}.csv
     fi
