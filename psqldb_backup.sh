3f7ee90271ae688a6a9c117602e65548d6b5ce71
Humam Rashid <humamrashid@gmail.com>
2019-12-03 21:28:39 -0500

    Inc. commit.

--- psqldb_backup.sh
+++ psqldb_backup.sh
@@ -4,11 +4,12 @@
 
 # psqldb_backup: a script for UNIX-like systems running bash to archive and
 # encrypt Postgresql database tables. Only works with psql and GnuPG. Further,
-# the script assumes GnuPG has been configured and a key pair already generated
-# for the recipient. Works on one database at a time but any number of tables
-# from the database can be supplied as arguments. First 5 arguments to the
-# script are database owner password, database owner name, database name, backup
-# path and recipient user-id (i.e., email address). Any arguments thereafter are
+# the script assumes GnuPG has been configured and a key pair already generated.
+
+# Works on one database at a time but any number of tables from the database can
+# be supplied as arguments. First 5 arguments to the script are database owner
+# password, database owner name, database name, backup path and recipient
+# user-id (i.e., email address), respectively. Any arguments thereafter are
 # considered to be the names of tables from the database. Each encrypted archive
 # for a particular table is saved in the backup directory with the original name
 # followed by a timestamp and ending '.tar.gz.gpg'.
@@ -22,16 +23,18 @@ if [ ! -d $4 ]; then
     echo "directory $4 does not exist!"
     exit 2
 fi
+cd $4
 for ((i=0; i<=$#; i++)); do
     if [ $i -gt 5 ]; then
         PGPASSWORD=$1 psql -U $2 -d $3 -c \
         "\copy ${!i} to stdout with (format csv, header true, delimiter ',');" \
-        > $4/${!i}.csv && \
-        tar cz $4/${!i}.csv | gpg --encrypt --recipient $5 \
-        > $4/${!i}.$(date '+%Y%m%d_%H%M%S').tar.gz.gpg && \
-        rm $4/${!i}.csv
+        > ${!i}.csv && \
+        tar cz ${!i}.csv | gpg --encrypt --recipient $5 \
+        > ${!i}.$(date '+%Y%m%d_%H%M%S').tar.gz.gpg && \
+        rm ${!i}.csv
     fi
 done
+cd -
 exit 0
 
 # EOF.
