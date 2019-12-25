e3e9e3a5c6f4c57d519e7deba809bb3e8c541c54
Humam Rashid <humamrashid@gmail.com>
2019-12-25 03:15:16 -0500

    Added WIP csv join program in Go.

---
+++ csv_join.go
@@ -0,0 +1,29 @@
+/* csv_join.go:
+ *
+ * An implementation of the UNIX join utility for CSV files. Written in Go for
+ * practice in the language.
+ */
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+)
+
+func hash_join() {
+	return
+}
+
+func merge_join() {
+	return
+}
+
+func nested_join() {
+	return
+}
+
+func main() {
+	fmt.Println("csv_join")
+}
+
+// EOF.
