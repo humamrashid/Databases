618524e2f0483f922234772c5f6dd6d1d6342a6c
Humam Rashid <humamrashid@gmail.com>
2019-12-25 01:58:44 -0500

    line-width change.

--- csv_join.rb
+++ csv_join.rb
@@ -1,31 +1,25 @@
 #!/usr/bin/ruby
 # Humam Rashid
 #
-# csv_join: A join implementation for CSV files.
-# Assumes both CSV files have same number of fields per
-# record and the join key is at the same position in each
-# record (if it exists). This implementation is essentially
-# a rudimentary version of the UNIX join utility specific to
-# CSV files. In keeping with the output of the UNIX join,
-# the join key is printed on the leftmost column but not in
-# the matched records.
+# csv_join: A join implementation for CSV files. Assumes both CSV files have same number of fields
+# per record and the join key is at the same position in each record (if it exists). This 
+# implementation is essentially a rudimentary version of the UNIX join utility specific to CSV
+# files. In keeping with the output of the UNIX join, the join key is printed on the leftmost column
+# but not in the matched records.
 #
 # CLI arguments:
 #   1.) < -h | -m | -n >, for join implementation type
 #   (hash, merge and nested-loop respectively).
 #
-#   2.) <key_index>, for index of key to join records on,
-#   (0 to number_of_fields - 1). Each key is assumed to be
-#   unique to a record in a table and each key is also
-#   assumed to be 'comparable' so records can be sorted (for
-#   merge joins).
+#   2.) <key_index>, for index of key to join records on, (0 to number_of_fields - 1). Each key is
+#   assumed to be unique to a record in a table and each key is also assumed to be 'comparable' so
+#   records can be sorted (for merge joins).
 #
 #   3.) <file1> <file2>, CSV files to join.
 
 require 'csv'
 
-# Adding methods to String class to check if a String value
-# is representing an integer or float.
+# Adding methods to String class to check if a String value is representing an integer or float.
 class String
   def int?
     Integer(self) && true rescue false
@@ -36,8 +30,8 @@ class String
   end
 end
 
-# Checks if a string field value is less than another
-# according to type represented (string, int or float).
+# Checks if a string field value is less than another according to type represented (string, int or
+# float).
 def less_than?(val1, val2)
   if val1.int?
     val1.to_i < val2.to_i
@@ -48,8 +42,8 @@ def less_than?(val1, val2)
   end
 end
 
-# Checks if a string field value is greater than another
-# according to type represented (string, int or float).
+# Checks if a string field value is greater than another according to type represented (string, int
+# or float).
 def greater_than?(val1, val2)
   if val1.int?
     val1.to_i > val2.to_i
@@ -79,10 +73,8 @@ def exit_if_empty(file)
   abort "Error: #{file} is empty!" if file.empty?
 end
 
-# hash_join() puts all records of the first CSV file into a
-# hashtable and compares against hashes of the join key for
-# records in the second CSV file. Output is not sorted on
-# join key.
+# hash_join() puts all records of the first CSV file into a hashtable and compares against hashes
+# of the join key for records in the second CSV file. Output is not sorted on join key.
 def hash_join(key_index, file1, file2)
   f1_htable = {}
   file1.each do |i|
@@ -95,8 +87,7 @@ def hash_join(key_index, file1, file2)
   end
 end
 
-# merge_sorter(): sorts CSV files for merge_join() by
-# sorting records in-place.
+# merge_sorter(): sorts CSV files for merge_join() by sorting records in-place.
 def merge_sorter(key_index, file1, file2)
   key = file1[0][key_index]
   if key.int?
@@ -114,9 +105,8 @@ def merge_sorter(key_index, file1, file2)
   end
 end
 
-# merge_join() assumes join key is 'comparable' and treats
-# the key as a string unless it represents a numeric type
-# (int or float). Output is sorted on join key.
+# merge_join() assumes join key is 'comparable' and treats the key as a string unless it represents
+# a numeric type (int or float). Output is sorted on join key.
 def merge_join(key_index, file1, file2)
   # Sorting part.
   merge_sorter(key_index, file1, file2)
@@ -133,16 +123,14 @@ def merge_join(key_index, file1, file2)
       puts "#{r[key_index]} #{r.reject { |e| e == r[key_index] }.to_csv.chomp} #{q.reject \
         { |e| e == r[key_index] }.to_csv.chomp}" unless r[key_index].nil? || q[key_index].nil?
       t = file2[k = j + 1]
-      # Check for further records that match with r on the
-      # join key.
+      # Check for further records that match with r on the join key.
       while k != file2.length && r[key_index] == t[key_index]
         puts "#{r[key_index]} #{r.reject { |e| e == r[key_index] }.to_csv.chomp} #{t.reject \
           { |e| e == r[key_index] }.to_csv.chomp}" unless r[key_index].nil? || t[key_index].nil?
         t = file2[k += 1]
       end
       s = file1[l = i + 1]
-      # Check for further records that match with q on the
-      # join key.
+      # Check for further records that match with q on the join key.
       while l != file1.length && q[key_index] == s[key_index]
         puts "#{q[key_index]} #{q.reject { |e| e == q[key_index] }.to_csv.chomp} #{s.reject \
           { |e| e == q[key_index] }.to_csv.chomp}" unless q[key_index].nil? || s[key_index].nil?
@@ -154,8 +142,8 @@ def merge_join(key_index, file1, file2)
   end
 end
 
-# nested_join() works ok for number of records upto the tens
-# of thousands. Output is not sorted on join key.
+# nested_join() works ok for number of records upto the tens of thousands. Output is not sorted on
+# join key.
 def nested_join(key_index, file1, file2)
   for i in file1
     for j in file2
